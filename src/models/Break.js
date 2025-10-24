9e8bf720deaee434decde1047092ba6693a33f0a
Denchikper <danman00@yandex.ru>
2025-10-24 18:03:22 +0200

    24.10.2025

--- src/models/Break.js
+++ src/models/Break.js
@@ -1,30 +1,27 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../database');
 
-const Schedule = sequelize.define('Schedule', {
+const Break = sequelize.define('Break', {
   id: {
     type: DataTypes.INTEGER,
     primaryKey: true,
     autoIncrement: true
   },
   name: {
-    type: DataTypes.STRING(100),
-    allowNull: false
+    type: DataTypes.STRING(50),
+    defaultValue: 'Перемена'
   },
-  description: {
-    type: DataTypes.TEXT
+  duration_minutes: {
+    type: DataTypes.INTEGER,
+    allowNull: false
   },
   created_at: {
     type: DataTypes.DATE,
     defaultValue: DataTypes.NOW
-  },
-  updated_at: {
-    type: DataTypes.DATE,
-    defaultValue: DataTypes.NOW
   }
 }, {
-  tableName: 'schedules',
+  tableName: 'breaks',
   timestamps: false
 });
 
-module.exports = Schedule;
+module.exports = Break;
